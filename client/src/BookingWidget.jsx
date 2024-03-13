6a55a13f8e88b1a830262cdda1918c80fdc27621
Ritam Bhowmick <iammritam11@gmail.com>
2024-03-14 00:18:55 +0530

    sixth chnages

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+import { differenceInCalendarDays } from 'date-fns';
+
 export default function BookingWidget({place}) {
+    const [checkIn, setCheckIn] = useState('');
+    const [checkOut, setCheckOut] = useState('');
+    const [numberOfGuests, setNumberOfGuests] = useState(1);
+    const [name, setName] = useState('');
+    const [mobile, setMobile] = useState('');
+
+    let numberOfNights = 0;
+
+    if(checkIn && checkOut) {
+        numberOfNights = differenceInCalendarDays(new Date(checkIn), new Date(checkOut));
+    }
+
     return(
         <div className="bg-white shadow p-4 rounded-2xl">
             <div className="text-2xl text-center">
@@ -8,21 +23,44 @@ export default function BookingWidget({place}) {
                 <div className="flex">
                     <div className="py-3 px-4">
                         <label>Check-in:</label>
-                        <input type="date"/>
+                        <input type="date" 
+                            value={checkIn} 
+                            onChange={e => setCheckIn(e.target.value)}/>
                     </div>
                     <div className="py-3 px-4 border-l">
                         <label>Check-Out:</label>
-                        <input type="date"/>
+                        <input type="date" 
+                            value={checkOut} 
+                            onChange={e => setCheckOut(e.target.value)}/>
                     </div>
                 </div>
                 <div>
                     <div className="py-3 px-4 border-t">
                         <label>Number of guests: </label>
-                        <input type="number"/>
+                        <input type="number" 
+                            value={numberOfGuests} 
+                            onChange={e => setNumberOfGuests(e.target.value)}/>
                     </div>
+                    {numberOfNights > 0 && (
+                        <div className="py-3 px-4 border-t">
+                            <label>Your full name: </label>
+                            <input type="text" 
+                                value={name} 
+                                onChange={e => setName(e.target.value)}/>
+                            <label>Phone Number: </label>
+                            <input type="tel" 
+                                value={mobile} 
+                                onChange={e => setMobile(e.target.value)}/>
+                        </div>
+                    )}
                 </div>
             </div>
-            <button className="primary mt-4">Book this place</button>
+            <button className="primary mt-4">
+            Book this places
+            {numberOfNights > 0 && (
+                <span> ₹{numberOfNights * place.price}</span>
+            )}
+            </button>
         </div>
     );
 }
